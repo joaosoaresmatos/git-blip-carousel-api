3b6e7e24981b462bf24bcac014b83e6ab4b3f92d
Joao Matos <joaosoaresmatos28@gmail.com>
2021-01-21 00:24:47 -0300

    refactoring the api code

--- src/pages/api/carousel.js
+++ src/pages/api/carousel.js
@@ -1,73 +1,56 @@
-async function carousel(request, response) {
+const carousel = async (request, response) => {
+    const NUM_ITEMS = 5;
+    const LANG = "C#";
 
+    const takeReposResponse = await fetch(`https://api.github.com/orgs/takenet/repos`);
+    const takeReposResponseJson = await takeReposResponse.json();
 
-    const gitReposResponse = await fetch(`https://api.github.com/orgs/takenet/repos`);
-    const gitReposResponseJson = await gitReposResponse.json();
-
-    const sortByCreateDate = (repos) => {
-
-        const sortedRepos = repos.sort((a, b) => Date.parse(a.created_at) - Date.parse(b.created_at));
-        return sortedRepos;
-    }
-
-    const filterReposByLang = (repos, lang) => {
-        
-        const filteredRepos = repos.filter(obj => obj.language === lang);
-        return filteredRepos;
-    }
-
-    const fillItem = (repos) => {
-
-        const items = repos.map(obj => ({
-            header: {
-                type: "application/vnd.lime.media-link+json",
-                value: {
-                    title: obj.name,
-                    text: obj.description,
-                    type: "image/jpeg",
-                    uri: obj.owner.avatar_url
-                }
-            },
-            options: [
-                {
-                    label: {
-                        type: "text/plain",
-                        value: "Finalizar"
-                    },
-                    value: {
-                        type: "application/json",
-                        value: {
-                            key1: "value1",
-                            key2: 2
-                        }
-                    }
-                }
-            ]
-        })
-        );
-
-        return items;
-
-    }
-
-
-    const createReposCarousel = (repos) => {
-
-        return {
-            itemType: "application/vnd.lime.document-select+json",
-            items: fillItem(repos)
-
-        }
-
-    }
-
-
-    const filtredRepos = sortByCreateDate(filterReposByLang(gitReposResponseJson, "C#")).slice(0, 5);
-    const carousel = createReposCarousel(filtredRepos);
+    const filteredRepos = filterReposByLang(takeReposResponseJson, LANG);
+    const sortedRepos = sortReposByCreatedDate(filteredRepos);
+    const carousel = createCarouselFromRepos(sortedRepos.slice(0, NUM_ITEMS));
 
     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-
     response.json(carousel);
 }
+
+const filterReposByLang = (repos, lang) => repos.filter(obj => obj.language === lang);
+
+const sortReposByCreatedDate = (repos) => repos.sort((a, b) => Date.parse(a.created_at) - Date.parse(b.created_at));
+
+const createCarouselFromRepos = (repos) => ({
+    itemType: "application/vnd.lime.document-select+json",
+    items: fillItems(repos),
+});
+
+const fillItems = (repos) => repos.map(obj => ({
+    header: createItemHeader(obj),
+    options: createItemOptions(),
+}));
+
+const createItemHeader = ({ name, description, owner: { avatar_url } }) => ({
+    type: "application/vnd.lime.media-link+json",
+    value: {
+        title: name,
+        text: description,
+        type: "image/jpeg",
+        uri: avatar_url,
+    },
+});
+
+const createItemOptions = () => [
+    {
+        label: {
+            type: "text/plain",
+            value: "Finalizar",
+        },
+        value: {
+            type: "application/json",
+            value: {
+                key1: "value1",
+                key2: 2,
+            },
+        },
+    },
+];
 
 export default carousel;
